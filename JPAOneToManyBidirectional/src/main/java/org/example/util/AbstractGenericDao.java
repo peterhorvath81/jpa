95775ae40d7d148b11c6719f1365ceeba2c4d252
Horváth <horvath.peter@marktsoft.hu>
2024-01-06 14:44:51 +0100

    entitymanagerutil added

--- JPAOneToManyBidirectional/src/main/java/org/example/util/AbstractGenericDao.java
+++ JPAOneToManyBidirectional/src/main/java/org/example/util/AbstractGenericDao.java
@@ -2,17 +2,9 @@ package org.example.util;
 
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.transaction.Transactional;
 
 public abstract class AbstractGenericDao<T>  implements GenericDao<T> {
-
-  private static final String PERSISTENCE_UNIT_NAME = "dvdrental";
-
-  private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-
-  private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 
   private Class<T> entityClass;
 
@@ -21,31 +13,28 @@ public abstract class AbstractGenericDao<T>  implements GenericDao<T> {
   }
 
   public T getById(Integer id) {
-    checkEntityManager();
-    return entityManager.find(entityClass, id);
+    return EntityManagerUtil.getEntityManager().find(entityClass, id);
   }
 
   public List<T> getAll() {
-      return entityManager.createQuery("from " + entityClass.getName()).getResultList();
+      return EntityManagerUtil.getEntityManager().createQuery("from " + entityClass.getName()).getResultList();
   }
 
   public void insert(T entity) {
-    checkEntityManager();
-    entityManager.getTransaction().begin();
-    entityManager.persist(entity);
-    entityManager.close();
+    EntityManagerUtil.beginTransaction();
+    EntityManagerUtil.persist(entity);
+    EntityManagerUtil.closeEntityManager();
   }
 
   public void delete(T entity) {
-    checkEntityManager();
-    entityManager.getTransaction().begin();
-    entityManager.remove(entity);
-    entityManager.close();
+    EntityManagerUtil.beginTransaction();
+    EntityManagerUtil.remove(entity);
+    EntityManagerUtil.closeEntityManager();
   }
 
   @Transactional
   public void update(Integer id, String propertyName, Object propertyValue) {
-    checkEntityManager();
+    EntityManager entityManager = EntityManagerUtil.getEntityManager();
     entityManager.getTransaction().begin();
     Object entity = entityManager.createNativeQuery("update address set " + propertyName + " = " + propertyValue.getClass() + " where address_id = " + id);
     entityManager.merge(entity);
@@ -53,13 +42,6 @@ public abstract class AbstractGenericDao<T>  implements GenericDao<T> {
   }
 
   public List<T> findByProperty(String propertyName, Object propertyValue) {
-    checkEntityManager();
-      return entityManager.createQuery("from " + entityClass.getName() + " where " + propertyName + " = " + propertyValue).getResultList();
-  }
-
-  private static void checkEntityManager() {
-    if (!entityManager.isOpen()) {
-      entityManager = entityManagerFactory.createEntityManager();
-    }
+      return EntityManagerUtil.getEntityManager().createQuery("from " + entityClass.getName() + " where " + propertyName + " = " + propertyValue).getResultList();
   }
 }
--- JPAOneToManyBidirectional/src/main/java/org/example/util/EntityManagerUtil.java
+++ JPAOneToManyBidirectional/src/main/java/org/example/util/EntityManagerUtil.java
@@ -1,4 +1,47 @@
 package org.example.util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class EntityManagerUtil {
+
+    private static final String PERSISTENCE_UNIT_NAME = "dvdrental";
+
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);;
+
+    private static final ThreadLocal<EntityManager> threadlocal = new ThreadLocal<>();
+
+    public static EntityManager getEntityManager() {
+        EntityManager entityManager = threadlocal.get();
+        if (entityManager == null) {
+            entityManager = entityManagerFactory.createEntityManager();
+            threadlocal.set(entityManager);
+        }
+        return entityManager;
+    }
+
+    public static void closeEntityManager() {
+        EntityManager entityManager = threadlocal.get();
+        if (entityManager != null) {
+            entityManager.close();
+            threadlocal.remove();
+        }
+    }
+
+    public static void beginTransaction() {
+        getEntityManager().getTransaction().begin();
+    }
+
+    public static void persist(Object object) {
+        getEntityManager().persist(object);
+    }
+
+    public static void remove(Object object) {
+        getEntityManager().remove(object);
+    }
+
+
+
+
 }
